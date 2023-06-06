79dcfdbb8c7ab8ff560c9de605204d51bc192688
Ruth James <jamesruth449@gmail.com>
2023-06-06 20:33:15 +0100

    first commit

---
+++ src/views/ContactView.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+    methods: {
+        flash(message) {
+             alert(message)
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="contact">
+        <h1>This is an contact page</h1>
+        <main>
+            <p>
+                <button @click="flash('It Works')">Click Me</button>
+            </p>
+        </main>
+    </div>
+</template>
+  
+<style>
+@media (min-width: 1024px) {
+    .contact {
+        min-height: 100vh;
+        display: flex;
+        align-items: center;
+    }
+}
+</style>
+  
---
+++ src/views/HomeView.vue
@@ -0,0 +1,29 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue';
+import { ref } from 'vue';
+
+let message =  ref('Hello RMMM!');
+
+let doSomething = () => {
+  alert("doing it now")
+};
+
+setTimeout(() => {
+  message.value = 'Saranghye Oppa'
+}, 2000);
+
+
+</script>
+
+<template>
+  <main>
+    <TheWelcome />
+    <p>{{ message }}</p>
+    <p>
+      <input type="text" v-model="message">
+    </p>
+    <p>
+      <button @click="doSomething">Click Me</button>
+    </p>
+  </main>
+</template>
