ef1fb769152811bc3e6b76f88c56539b2b23bbe1
Ruth James <jamesruth449@gmail.com>
2023-06-10 21:11:22 +0100

    Initialized team dashboard excercise

---
+++ src/components/Team/TeamHeader.vue
@@ -0,0 +1,22 @@
+<script setup>
+defineProps({
+    team: Object
+})
+</script>
+<template>
+     <header class="flex justify-between mt-3">
+        <div>
+            <button class="bg-purple-500 hover:bg-purple-600 text-white px-4 py-5 rounded disabled:bg-gray-400"
+                :disabled="team.members.length === team.spots">Add Member ({{ team.spots - team.members.length }} spots left)</button>
+        </div>
+        <div>
+            <h3 class="inline-flex items-center text-3xl relative">
+                <img src="@/assets/images/bts-logo.png" alt="bts-logo" class="w-12 h-10 mr-2">
+                {{ team.name }}
+                <div
+                    class="bg-purple-400 w-5 h-5 text-xs text-white rounded-full flex justify-center items-center absolute -right-4 -top-2">
+                    {{ team.spots }}</div>
+            </h3>
+        </div>
+    </header>
+</template>
---
+++ src/views/TeamView.vue
@@ -0,0 +1,31 @@
+<script setup>
+import TeamHeader from '../components/Team/TeamHeader.vue';
+import TeamMember from '../components/Team/TeamMember.vue';
+import team from '@/team.json'
+</script>
+<template>
+   <TeamHeader :team="team" />
+    <div class="grid">
+        <div class="place-self-center flex flex-col gap-y-3" style="width: 725px;">
+            <table class="table-fixed border-spacing-2 border-separate">
+                <thead>
+                    <th class="text-left px-6 py-6">Name</th>
+                    <th class="text-left px-6 py-6">Email</th>
+                    <th class="text-left px-6 py-6">Status</th>
+                </thead>
+
+                <tbody>
+                    <TeamMember v-for="member in team.members" :name=member.name :email=member.email :status=member.status>
+                        <img :src="member.img" alt="rm-image" class="w-14 h-14 rounded-full" />
+                    </TeamMember>
+                </tbody>
+            </table>
+            <p class="text-right text-gray-600 italic" v-show="team.members.length === team.spots">There are no remaining spots. Upgrade to add more.</p>
+        </div>
+    </div>
+    <footer class="mt-12 bg-gray-100 py-4 text-center text-black">
+        <h5 class="font-semibold text-lg">
+            {{ team.name }} - {{ team.spots }} Member Team
+        </h5>
+    </footer>
+</template>
