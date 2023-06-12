a93c6158f7a30284cbcf3407d445376a79209066
Ruth James <jamesruth449@gmail.com>
2023-06-12 19:59:55 +0100

    complete learn vue 3 step by step

--- src/views/TeamView.vue
+++ src/views/TeamView.vue
@@ -2,32 +2,19 @@
 import TeamHeader from '../components/Team/TeamHeader.vue';
 import TeamMembers from '../components/Team/TeamMembers.vue';
 import TeamFooter from '../components/Team/TeamFooter.vue';
-import Modal from '../components/Modal.vue';
 import { useTeamStore } from '../stores/TeamStore';
-import { ref } from "vue";
 
 let team = useTeamStore();
 team.fill();
 setTimeout(() => {
    team.grow(8)
 }, 2000);
-let showModal = ref(false);
+
 
 </script>
 <template>
-   <TeamHeader @add="showModal = true" />
+   <TeamHeader />
    <TeamMembers />
    <TeamFooter />
-   <Modal :show="showModal" @close="showModal = false">
-      <template #default>
-         <p>Need to add a new member to your team?</p>
-         <form class="mt-6">
-            <div class="flex gap-2">
-               <input type="email" placeholder="Email Address..." class="flex-1">
-               <button>Add</button>
-            </div>
-         </form>
-      </template>
 
-   </Modal>
 </template>
